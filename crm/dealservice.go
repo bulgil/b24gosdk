53d532b017f644b46f8c9464d2d65b5e89958fdb
bulgil <bulgilmutdinoff@yandex.ru>
2025-06-02 14:52:45 +0300

    transport refactor

--- crm/dealservice.go
+++ crm/dealservice.go
@@ -4,9 +4,9 @@ type DealService struct {
 	CRMService[Deal]
 }
 
-func NewDealService(client client, webhook string) *DealService {
+func NewDealService(transport Transport, webhook string) *DealService {
 	return &DealService{
-		CRMService: NewCrmService[Deal](client, webhook, methods{
+		CRMService: NewCrmService[Deal](transport, webhook, methods{
 			add:    "crm.deal.add",
 			get:    "crm.deal.get",
 			update: "crm.deal.update",
