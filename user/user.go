c9bac01a0b3e25bb4c2009e9d8530384adc1e347
bulgil <bulgilmutdinoff@yandex.ru>
2025-09-01 10:24:25 +0300

    added last name in user

--- user/user.go
+++ user/user.go
@@ -3,6 +3,7 @@ package user
 import "github.com/bulgil/b24gosdk"
 
 type User struct {
-	ID   b24gosdk.B24int `json:"ID"`
-	Name string          `json:"NAME"`
+	ID       b24gosdk.B24int `json:"ID"`
+	Name     string          `json:"NAME"`
+	LastName string          `json:"LAST_NAME"`
 }
